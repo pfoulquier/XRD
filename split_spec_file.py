2bd6df0b440c6f2f0d33c12fd5b6bcffa7ee1c2f
pfoulquier <97919781+pfoulquier@users.noreply.github.com>
2022-01-17 22:58:37 +0100

    Add files via upload

---
+++ split_spec_file.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+@author: P.Foulquier
+"""
+
+import numpy
+import re
+
+spec=['']
+    
+spec=open("20210636_Foulquier_BetaTest.txt").read() #Read spec file
+a=str(spec)
+
+x = [_.start() for _ in re.finditer('#S', a)] #Locate the start of the file name
+
+y = [_.start() for _ in re.finditer('#D', a)] #Locate the end of the file name
+
+z = [_.start() for _ in re.finditer('#L', a)] #Locate the beginning of the date
+
+
+
+i=0
+
+while i<len(x)-1:
+     w=open(a[x[i]:y[i]-1]+'.txt', 'w') 
+     w.write(a[z[i]+2:x[i+1]-1])  #Create text file with name and type of scan
+     w.close()
+     i+=1
+     print(i)
+     
+w=open(a[x[i]:y[i]-1]+'.txt', 'w') #Create the last scan file
+w.write(a[z[i]+2:len(a)])
+w.close()
